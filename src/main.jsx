5e7249af5323de34432e6588920f55fe0390b01b
dpatil1719 <patildhanashree888@gmail.com>
2025-12-16 13:51:26 -0800

    Integrate Atatus (RUM) in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// --- Atatus (Browser) ---
+import * as atatus from 'atatus-spa';
+atatus.config('c9f5afcebbc946b0a4a747899f2ff1cf').install();
+// Optional: temporarily test the integration, then remove after you see it in Atatus
+// atatus.notify(new Error('Test Atatus Setup'));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
